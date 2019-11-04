7402e7f6ce5e752a5541c397b38ecd43b7f087af
Regino P <regino5298@gmail.com>
2019-11-04 16:09:55 -0700

    init commit, met MVP

--- src/BottomRow.js
+++ src/BottomRow.js
@@ -1,24 +1,34 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 
 const BottomRow = () => {
+
+  const [down, downAmount] = useState(0);
+  const [quarter, quarterAmount] = useState(0);
+  const [toGo, toGoAmount] = useState(0);
+  const [ballOn, ballOnAmount] = useState(0);
+
   return (
     <div className="bottomRow">
       <div className="down">
         <h3 className="down__title">Down</h3>
-        <div className="down__value">3</div>
+        <div className="down__value">{down}</div>
+        <button onClick={() => downAmount(down + 1)} className="down__add">Down Add</button>
+        <button onClick={() => downAmount(0)} className="down__reset">Down Reset</button>
       </div>
       <div className="toGo">
         <h3 className="toGo__title">To Go</h3>
-        <div className="toGo__value">7</div>
+        <div className="toGo__value">{toGo}</div>
       </div>
       <div className="ballOn">
         <h3 className="ballOn__title">Ball on</h3>
-        <div className="ballOn__value">21</div>
+        <div className="ballOn__value">{ballOn}</div>
       </div>
       <div className="quarter">
         <h3 className="quarter__title">Quarter</h3>
-        <div className="quarter__value">4</div>
+        <div className="quarter__value">{quarter}</div>
+        <button onClick={() => quarterAmount(quarter + 1)} className="quarter__add">Quarter Add</button>
+        <button onClick={() => quarterAmount(0)} className="quarter__reset">Quarter Reset</button>
       </div>
     </div>
   );
